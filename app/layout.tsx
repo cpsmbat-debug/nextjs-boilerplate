1051e838dabc6e7d3504a80200012591bf8eac63
cpsmbat-debug <cpsmbat@gmail.com>
2025-09-03 23:31:53 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,13 @@ export default function RootLayout({ children }) {
   gtag('js', new Date());
 
   gtag('config', 'AW-17516892111');
-</script>  <head>
+</script> <!-- Event snippet for Achat conversion page -->
+<script>
+  gtag('event', 'conversion', {
+      'send_to': 'AW-17516892111/9j4ACKTdgJAbEM-f2qBB',
+      'transaction_id': ''
+  });
+</script> <head>
         {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17516892111"></script>
         <script dangerouslySetInnerHTML={{
@@ -29,8 +35,20 @@ export default function RootLayout({ children }) {
   gtag('js', new Date());
 
   gtag('config', 'AW-17516892111');
-</script></head>
+</script><<!-- Event snippet for Achat conversion page -->
+<script>
+  gtag('event', 'conversion', {
+      'send_to': 'AW-17516892111/9j4ACKTdgJAbEM-f2qBB',
+      'transaction_id': ''
+  });
+</script>head>
       <body className={inter.className}>{children}</body>
     </html>
   );
-}
+}<!-- Event snippet for Achat conversion page -->
+<script>
+  gtag('event', 'conversion', {
+      'send_to': 'AW-17516892111/9j4ACKTdgJAbEM-f2qBB',
+      'transaction_id': ''
+  });
+</script>
